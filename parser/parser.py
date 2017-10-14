e9ca6c7d84961842a32aa5c9eb52fc1a74ec6b30
sijmenhuizenga <sijmenhuizenga@gmail.com>
2017-10-14 16:43:16 +0200

    add mocked parser

---
+++ parser/parser.py
@@ -0,0 +1,28 @@
+def parsehtml(html):
+    return (
+        "Jongeren delen massaal wraakporno nu het nog mag",
+        [
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla et dignissim dui. Ut dictum nunc mattis est "
+            "maximus, eu dictum magna laoreet. Vivamus condimentum, enim fermentum consectetur sodales, justo sem iaculis "
+            "ante, quis facilisis orci justo vel diam. Maecenas nec velit lectus. Vestibulum sagittis volutpat enim non "
+            "maximus. Etiam consequat pellentesque tellus, nec pulvinar nunc gravida a. Ut ornare metus ut dolor pretium "
+            "pretium. Curabitur accumsan risus a arcu malesuada tempor. Vestibulum eget mauris id mi aliquam consequat nec "
+            "a lorem. Suspendisse pellentesque nec mauris eget tincidunt. Phasellus ac ipsum vitae felis malesuada congue vitae vel purus.",
+
+            "Sed augue nisl, tempor in nisl id, tempor volutpat turpis. Donec accumsan volutpat diam sit amet condimentum. "
+            "Donec gravida mi et massa pretium congue. Nullam ornare erat ac turpis porttitor porta. Aenean porttitor vestibulum"
+            " rutrum. Sed elementum vitae dolor sit amet convallis. Mauris dolor turpis, vulputate ultricies nisi ut, ornare"
+            " tempus urna. Morbi risus odio, luctus sit amet viverra sit amet, sollicitudin eget ligula. Sed a diam iaculis,"
+            " porttitor lectus vel, tempor massa. In ligula mi, ultrices ut dui at, sodales scelerisque mauris. Proin "
+            "fermentum commodo tempus.",
+
+            "Nullam semper pulvinar diam ut scelerisque. Donec ex libero, venenatis eget suscipit ac, maximus at risus. "
+            "Aenean felis lorem, semper egestas molestie nec, congue eu neque. Fusce pellentesque tincidunt faucibus. Donec "
+            "porttitor, nunc eu vehicula blandit, tortor massa posuere ligula, eget dictum velit eros eu est. Sed tincidunt"
+            " lorem suscipit, pellentesque nisl vitae, gravida leo. Curabitur interdum nulla ipsum, quis egestas nisl iaculis"
+            " ut. Integer a nunc vitae dolor eleifend ultrices non eget nibh. Aenean eget pellentesque ipsum.",
+        ]
+    )
+
+
+parsehtml("test")
