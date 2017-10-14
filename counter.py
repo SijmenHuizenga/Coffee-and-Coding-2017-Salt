52f7ec3e0f3499756938465b7121a570c2931a02
ashish <ashish.khanal@hotmail.com>
2017-10-15 00:54:43 +0200

    Update counter.py

--- counter.py
+++ counter.py
@@ -3,6 +3,12 @@ dict = {}
 path1  = "/csr_files"
 path2 = "/non_csr_files" 
 file1 = open('results.txt', 'w')
+mostusedwords = ["the", "be", "to", "of", "and", "a", "in", "that", "have", "I", "it", "for", "not", "on", "with", "he", "as", "you", "do", "at", "this", "but", 
+		"his", "by", "from", "they", "we", "say", "her", "she", "or", "an", "will", "my", "one", "all", "would", "there", "their", "what", "so", "up", 
+		"out", "if","about", "who", "get", "which", "go", "me", "when", "make", "can", "like", "time", "no", "just", "him", "know", "take", "people", 
+		"into", "year", "your", "good", "some", "could", "them", "see", "other", "than", "then", "now", "look", "only", "come", "its", "over", "think",
+		"also", "back", "after", "use", "two", "how", "our", "work", "first", "well", "way", "even", "new", "want", "because", "any", "these", "give", 
+		"day", "most", "us"]
 
 for file_ in os.listdir(path1):
 	file_ = open(file_, 'r')
@@ -10,16 +16,19 @@ for file_ in os.listdir(path1):
 	for line in file_:
 		line = line.split()
 		for word in line:
-			if dict.has_key(word):
-				dict[word] += 1
-			else:
-				dict[word] = 0 
+			for word not in mostusedwords:
+				if dict.has_key(word):
+					dict[word] += 1
+				else:
+					dict[word] = 0 
 
 list = dict.items()
 for key,value in dict.iems():
 	file1.write("%s:%s\n" %(key,value))
 
 file1.close()
+
+
 
 
 
